91a4a6482856873319568e40f7d6fe7e0930b84c
Terry Gluff <terry@pclanservices.com>
2019-09-01 21:44:02 -0500

    locations needed for backup script

--- Backup.sh
+++ Backup.sh
@@ -1,3 +1,36 @@
 ## This backs up all programs relevant config files, databases and whatever else
 
 ~ /.config
+
+sonarr
+https://github.com/Sonarr/Sonarr/wiki/Backup-and-Restore
+/home/plex/.config/NzbDrone/Backups/scheduled/
+
+radarr
+/home/plex/.config/Radarr/Backups/scheduled/
+
+lidarr
+/home/plex/.config/Lidarr/Backups/scheduled/
+
+nzbget
+/opt/nzbget/nzbget.conf
+
+deluge
+/home/plex/.config/deluge
+
+mylar
+/opt/Mylar/mylar.db
+/opt/Mylar/config.ini
+
+plex
+/var/lib/plexmediaserver/Library/Application Support/Plex Media Server
+
+jackett
+/home/plex/.config/Jackett/ServerConfig.json
+/home/plex/.config/Jackett/Indexers
+NOT SURE IF THIS APPLIES TO US****
+You also should backup ~/.config/.mono/keypairs/
+Without the keystore it's not possible to decrypt passwords in the config files making them useless.
+
+headphones
+/opt/headphones/config.ini
